49f5cef1ae3304d944c70ec40a118f2a1716caad
Zoltan Dalmadi <dmz985@gmail.com>
2020-03-09 16:55:20 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@shopify/polaris/styles.css";
+// import "./index.css";
+// import App from "./App";
+import { AppProvider, Card } from "@shopify/polaris";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// If the current window is the 'parent', change the URL by setting location.href
+// if (window.top === window.self) {
+//   window.location.assign(`https://${shopOrigin}/admin/apps/${permissionUrl}`);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If the current window is the 'child', change the parent's URL with Shopify App Bridge's Redirect action
+// } else {
+//   const app = createApp({
+//     apiKey,
+//     shopOrigin: "tutorial-mark.myshopify.com"
+//   });
+
+//   Redirect.create(app).dispatch(
+//     Redirect.Action.ADMIN_PATH,
+//     permissionUrl
+//   );
+function MyApp() {
+  return (
+    <AppProvider>
+      <Card title="Online store dashboard" sectioned>
+        <p>View a summary of your online store’s performance.</p>
+      </Card>
+    </AppProvider>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
